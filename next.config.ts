fa419199e14b171f5fb06e0bff7b9ddfb06650ae
BidaoVIP <fabriciofagundes238@gmail.com>
2025-10-06 12:41:59 -0300

    nao mais estatico

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  basePath: "/my-app",
-  assetPrefix: "/my-app/",
+  reactStrictMode: true,
 };
 
 export default nextConfig;
