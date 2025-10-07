912d652341c193f1b699b70df9b70098115a7fd8
BidaoVIP <fabriciofagundes238@gmail.com>
2025-10-07 12:02:38 -0300

    adicionando motion e correção da dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import { motion } from "framer-motion";
 import { supabase } from "@/supabaseClient";
 
 interface Usuario {
@@ -25,14 +26,14 @@ export default function Dashboard() {
 
       const { data: profile, error: profileError } = await supabase
         .from("profiles")
-        .select("name_user")
+        .select("user_name")
         .eq("id", authData.user.id)
         .single();
 
       setUsuario({
         id: authData.user.id,
-        email: authData.user.email ?? null, 
-        nome: profile?.name_user ?? authData.user.email ?? "Usuário",
+        email: authData.user.email ?? null,
+        nome: profile?.user_name ?? authData.user.email ?? "Usuário",
       });
 
       setLoading(false);
@@ -50,19 +51,34 @@ export default function Dashboard() {
   }
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-gray-700 to-black relative">
-      <div className="absolute inset-0 bg-[url('/carro-bg.jpg')] bg-cover bg-center"></div>
+    <div className="relative min-h-screen flex flex-col items-center justify-center text-center text-white overflow-hidden">
+      <div className="absolute inset-0">
+        <img
+          src="/img/carro-bg.jpeg"
+          alt="CORVETTE C5"
+          className="w-full h-full object-cover"
+        />
+        <div className="absolute inset-0 bg-black/70"></div>
+      </div>
 
-      <h1 className="text-red-600 text-4xl font-extrabold tracking-widest z-10">
+      <motion.h1
+        initial={{ scale: 2, opacity: 0 }}
+        animate={{ scale: 1, opacity: 1 }}
+        transition={{ duration: 1.2, ease: "easeOut" }}
+        className="text-red-600 text-4xl md:text-6xl font-extrabold tracking-widest drop-shadow-lg"
+      >
         {`Bem-vindo, ${usuario?.nome}!`}
-      </h1>
+      </motion.h1>
 
-      <button
-        className="mt-6 px-6 py-3 rounded-full bg-red-600 text-white font-bold shadow-4xl hover:bg-red-700 transition z-10"
+      <motion.button
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 1 }}
+        transition={{ delay: 1, duration: 0.6 }}
+        className="mt-8 px-8 py-3 rounded-full bg-red-600 text-white font-bold shadow-lg hover:bg-red-700 transition"
         onClick={() => router.push("/garagem")}
       >
         Entrar na Garagem
-      </button>
+      </motion.button>
     </div>
   );
 }
